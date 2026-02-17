74b99375bf2571b3c9eb3ef6161061ea97cdc8c0
ablncode <a.bilun@eurojet.com.ua>
2026-02-17 15:39:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const cors = require('cors');
 
+const express = require('express');
+const cors = require('cors'); // Перевірте, чи є цей рядок
+const app = express();
+
+app.use(cors()); // ЦЕ ОБОВ'ЯЗКОВО! Дозволяє Lampa підключатися до сервера
+
 const app = express();
 app.use(cors()); // Allow requests from any device
 
